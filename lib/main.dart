5d3f37e276e736552bfcb028a80fdd7489f504df
Sooraj S <52352285+Sooraj-s-98@users.noreply.github.com>
2021-11-04 15:07:32 +0530

    initial

--- lib/main.dart
+++ lib/main.dart
@@ -23,8 +23,38 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.amber,
-      appBar: AppBar(),
-    );
+        backgroundColor: Colors.amber,
+        body: SafeArea(
+            child: Container(
+          width: double.infinity,
+          height: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                "Welcome",
+                style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 50,
+                    fontWeight: FontWeight.bold),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                      onPressed: () {}, child: const Text("click me")),
+                  const Text(
+                    "Sooraj s",
+                    style: TextStyle(
+                        color: Colors.red,
+                        fontSize: 50,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        )));
   }
 }
